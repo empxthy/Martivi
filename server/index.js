25938c9c943db1924e1c5b8b6046851fde20e4fa
empxthy <landshok35@gmail.com>
2024-12-25 17:53:56 +0400

    Added functionality

--- server/index.js
+++ server/index.js
@@ -1,30 +1,32 @@
-    require('dotenv').config();
-    const express = require('express');
-    const cors = require('cors');
-    const app = express();
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const app = express();
 
-    const ACCESS_KEY = process.env.ACCESS_KEY.split(',');
-    const PORT = process.env.PORT || 3000;
+const config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
 
-    app.use(cors());
-    app.use(express.json());
+const ACCESS_KEYS = config.ACCESS_KEYS;
+const PORT = config.PORT || 3000;
 
-    app.get('/', (req, res) => {
-        res.send("Hello, World!");
-    })
+app.use(cors());
+app.use(express.json());
 
-    app.post('/rest/v2/api/validate', (req, res) => {
-        const { accessKey } = req.body;
+app.post("/rest/v2/api/validate", (req, res) => {
+    const { accessKey } = req.body;
 
-        console.log(accessKey);
+    const foundKey = ACCESS_KEYS.find((item) => item.key === accessKey);
 
-        if (ACCESS_KEY.includes(accessKey)) {
-            res.status(200).json({ message: "Access Key accepted!", isValid: true })
-        } else {
-            return res.status(400).json({ message: "Invalid Access Key!", isValid: false });
-        }
-    });
+    if (foundKey) {
+        res.status(200).json({
+            message: "Access Key accepted!",
+            isValid: true,
+            role: foundKey.role,
+        });
+    }else {
+        res.status(400).json({ message: "Invalid Access Key!", isValid: false });
+    }
+});
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
